fd9f1340cf835f28c2646ca4052dfb46192e7388
Ulearn 2.1-2.14 Юнусов ПрИ-202 <lionalex02@ya.ru>
2024-11-08 02:31:16 +0500

    Ulearn 7.1-7.4 Юнусов ПрИ-202

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,7 @@ package com.example.task02;
 
 import java.io.IOException;
 
+
 public class Task02Main {
     public static void main(String[] args) throws IOException {
         // чтобы протестировать свое решение, вам нужно:
@@ -9,5 +10,15 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+
+        int inpOne =   System.in.read();
+        while (inpOne != -1) {
+            int inpTwo = System.in.read();
+            if (inpTwo != 10 || inpOne != 13) {
+                System.out.write(inpOne);
+            }
+            inpOne = inpTwo;
+        }
+        System.out.flush();
     }
 }
